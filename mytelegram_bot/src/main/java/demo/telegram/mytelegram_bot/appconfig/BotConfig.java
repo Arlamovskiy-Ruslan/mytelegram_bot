2553277a251869c4296fc48af01291dbe7ef2f36
Arlamovskiy-Ruslan <55766007+Arlamovskiy-Ruslan@users.noreply.github.com>
2021-04-04 16:09:56 +0300

    Initial commit

--- mytelegram_bot/src/main/java/demo/telegram/mytelegram_bot/appconfig/BotConfig.java
+++ mytelegram_bot/src/main/java/demo/telegram/mytelegram_bot/appconfig/BotConfig.java
@@ -4,8 +4,10 @@ import demo.telegram.mytelegram_bot.MyTelegramBot;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.telegram.telegrambots.bots.DefaultBotOptions;
 import org.telegram.telegrambots.meta.ApiContext;
 
@@ -23,7 +25,7 @@ public class BotConfig {
     private int proxyPort;
 
     @Bean
-    public MyTelegramBot MySuperTelegramBot(){
+    public MyTelegramBot mySuperTelegramBot(){
         DefaultBotOptions options = ApiContext
                 .getInstance(DefaultBotOptions.class);
 
@@ -37,5 +39,14 @@ public class BotConfig {
         mySuperTelegramBot.setWebHookPath(webHookPath);
 
         return mySuperTelegramBot;
+    }
+
+    @Bean
+    public MessageSource messageSource(){
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+
+        messageSource.setBasename("classpath:messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
     }
 }
