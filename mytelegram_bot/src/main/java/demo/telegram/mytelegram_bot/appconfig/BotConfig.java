4e83e631df82272e87be1f4acfc887b6edfa8fb9
Arlamovskiy-Ruslan <55766007+Arlamovskiy-Ruslan@users.noreply.github.com>
2021-04-04 14:51:59 +0300

    Initial commit

--- mytelegram_bot/src/main/java/demo/telegram/mytelegram_bot/appconfig/BotConfig.java
+++ mytelegram_bot/src/main/java/demo/telegram/mytelegram_bot/appconfig/BotConfig.java
@@ -1,14 +1,41 @@
 package demo.telegram.mytelegram_bot.appconfig;
 
+import demo.telegram.mytelegram_bot.MyTelegramBot;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.telegram.telegrambots.bots.DefaultBotOptions;
+import org.telegram.telegrambots.meta.ApiContext;
 
 @Getter
 @Setter
 @Configuration
 @ConfigurationProperties(prefix = "telegrambot")
 public class BotConfig {
+    private String webHookPath;
+    private String botUserName;
+    private String botToken;
 
+    private DefaultBotOptions.ProxyType proxyType;
+    private String proxyHost;
+    private int proxyPort;
+
+    @Bean
+    public MyTelegramBot MySuperTelegramBot(){
+        DefaultBotOptions options = ApiContext
+                .getInstance(DefaultBotOptions.class);
+
+        options.setProxyHost(proxyHost);
+        options.setProxyPort(proxyPort);
+        options.setProxyType(proxyType);
+
+        MyTelegramBot mySuperTelegramBot = new MyTelegramBot(options);
+        mySuperTelegramBot.setBotUserName(botUserName);
+        mySuperTelegramBot.setBotToken(botToken);
+        mySuperTelegramBot.setWebHookPath(webHookPath);
+
+        return mySuperTelegramBot;
+    }
 }
